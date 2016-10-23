dec798c91a3630303277e79a6e8116b529125c6f
Hari Gopal <mail@harigopal.in>
2016-10-23 12:38:20 +0530

    Values

---
+++ 02-values.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello " + "world!")
+	fmt.Println("100 - 98 =", 100 - 98)
+	fmt.Println("10.0 / 3.0 =", 10.0 / 3.0)
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
