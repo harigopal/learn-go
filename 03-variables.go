4d0f3c1aff742b8d727c267abe70d82bb7ed8392
Hari Gopal <mail@harigopal.in>
2016-10-23 12:43:43 +0530

    Variables

---
+++ 03-variables.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+  var helloWorld string = "Hello world!"
+  fmt.Println(helloWorld)
+
+  var firstNumber, secondNumber int = 99, 999
+  fmt.Println(firstNumber, secondNumber)
+
+  var boolean = true
+  fmt.Println(boolean)
+
+  var someInteger int
+  fmt.Println(someInteger)
+
+  figureItOut := "Hello again!"
+  fmt.Println(figureItOut)
+}
