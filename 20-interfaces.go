022e22520ab47ac3a90ae6e653a3d39d95897582
Hari Gopal <mail@harigopal.in>
2016-11-23 20:32:44 +0530

    Interfaces

---
+++ 20-interfaces.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+import "math"
+
+type geometry interface {
+	area() float64
+	perimeter() float64
+}
+
+type rectangle struct {
+	width, height float64
+}
+
+type circle struct {
+	radius float64
+}
+
+func (r rectangle) area() float64 {
+	return r.width * r.height
+}
+
+func (r rectangle) perimeter() float64 {
+	return 2 * (r.width + r.height)
+}
+
+func (c circle) area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+func (c circle) perimeter() float64 {
+	return 2 * math.Pi * c.radius
+}
+
+func measure(g geometry) {
+	fmt.Println(g)
+	fmt.Println(g.area())
+	fmt.Println(g.perimeter())
+}
+
+func main() {
+	testRectangle := rectangle{width: 3.14, height: 3.14}
+	testCircle := circle{radius: 3.14}
+	measure(testRectangle)
+	measure(testCircle)
+}
