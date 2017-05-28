c51e7369c4770b312efc58ac2484c6b9c9872dbc
Hari Gopal <mail@harigopal.in>
2017-05-28 14:25:40 +0530

    Sorting

---
+++ 39-sorting.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"sort"
+	"fmt"
+)
+
+func main() {
+	words := []string{"world", "go", "hello"}
+	sort.Strings(words)
+	fmt.Println("Sorted words:", words)
+
+	numbers := []int{42, 2, 4}
+	sort.Ints(numbers)
+	fmt.Println("Numbers:", numbers)
+
+	areNumbersSorted := sort.IntsAreSorted((numbers))
+	fmt.Println("Numbers Sorted?", areNumbersSorted)
+}
